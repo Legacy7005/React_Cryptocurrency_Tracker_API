79cc600dd87d7b444df658faddb10b2de738175c
Clinton F <legacy_7005@yahoo.com>
2021-07-09 20:27:00 -0400

    Getting Data Back!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+// 
+
 function App() {
+
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+      axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      });
+  });
+
   return (
     <div className="App">
       <h1>💰 React Cryptocurrecy API ✈</h1>
